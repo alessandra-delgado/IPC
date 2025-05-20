59772005b1bcea2525c386db98eb97bcbaac45f5
MatildeS <matilde.sequeira@ubi.pt>
2025-05-20 18:07:27 +0100

    fixed loop game

--- client/src/client.cpp
+++ client/src/client.cpp
@@ -1,10 +1,13 @@
 // C++ Program for Message Queue (Writer Process)
-#include <sys/ipc.h>
 #include <sys/msg.h>
 #include <iostream>
+#include <limits>
 #include <cstring>
+#include <sys/ipc.h>
 #include <unistd.h>
+#include <cstdlib>
 #include "../../shared/include/msg_t.hpp"
+#include "../../server/include/protocol.hpp"
 
 using namespace std;
 
@@ -16,62 +19,84 @@ int main()
     // ftok to generate unique key
     key_t key = ftok("tictactoe_connect", 1);
 
-    // msgget creates a message queue
-    // and returns identifier
+    // msgget creates a message queue and returns identifier
     int msgid = msgget(key, 0666 | IPC_CREAT);
-    message.msg_type = 1;
+    if (msgid == -1) {
+        perror("msgget");
+        return 1;
+    }
 
+
+    message.msg_type = 1;
+    message.sender_id = client_pid;
     // Envia PID
     snprintf(message.msg_text, sizeof(message.msg_text), "%d", client_pid);
 
-    msgsnd(msgid, &message, sizeof(msg_t) - sizeof(long), 0);
-    cout << "Client connected " << client_pid << endl;
-    memset(&message, 0, sizeof(message));
+    if (msgsnd(msgid, &message, sizeof(msg_t) - sizeof(long), 0) == -1) {
+        perror("msgsnd");
+        return 1;
+    }
 
-    message.msg_type = client_pid;
+    cout << "Client connected " << client_pid << endl;
 
     // Get game session PID
-    msgrcv(msgid, &message, sizeof(msg_t) - sizeof(long), client_pid, 0);
+    if (msgrcv(msgid, &message, sizeof(msg_t) - sizeof(long), client_pid, 0) == -1) {
+        perror("msgrcv failed");
+        return 1;
+    }
     
     int session_id = message.session_id;
-    cout << "Session ID: " << session_id << endl;
-    cout << "Game session established: " << message.msg_text << endl;
+
+    cout << "You: " << message.msg_text << endl;
 
     // todo: Game loop
     while(true){ // im just testing if i can correnctly send messages between clients and session threads :)
-        cout << "Waiting for server message..." << endl;
 
-        msgrcv(msgid, &message, sizeof(msg_t) - sizeof(long), 0, 0) == -1;
-        memset(&message, 0, sizeof(message));
-
-        cout << "DEBUG - Received: " << message.msg_text << endl;
-
-        if (strstr(message.msg_text, "MOVE")) {
+        string msg_text(message.msg_text);
+        
+        if (msg_text.find(protocol_to_str(Protocol::MSG_MOVE)) != string::npos){
             cout << "\nYour turn! Enter position (1-9):" << endl;
-            int position;
-            cin >> position;
-            cin.ignore();
-            message.msg_type = session_id;
-            snprintf(message.msg_text, sizeof(message.msg_text), "%d", position);
-            if (msgsnd(msgid, &message, sizeof(message), 0) == -1) {
-                perror("Failed to send move");
+            int position;   
+            while (!(cin >> position) || position < 1 || position > 9) {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid input. Please enter a number between 1 and 9: ";
             }
-        } else if (strstr(message.msg_text, "BOARD")) {
+
+            // message to server
+            memset(&message, 0, sizeof(message));
+            message.msg_type = session_id;
+            message.sender_id = client_pid;
+            snprintf(message.msg_text, sizeof(message.msg_text), "%d", position);
+
+            if (msgsnd(msgid, &message, sizeof(msg_t) - sizeof(long), 0) == -1) {
+                perror("msgsnd failed");
+                break;
+            }
+
+        } else if (msg_text.find(protocol_to_str(Protocol::MSG_BOARD)) != string::npos) {
             char* board = strchr(message.msg_text, '\n');
             if (board) cout << "Board:\n" << board + 1 << endl;
-        } else if (strstr(message.msg_text, "WAIT")) {
+        } else if (msg_text.find(protocol_to_str(Protocol::MSG_WAIT)) != string::npos) {
             cout << "Waiting for the other player..." << endl;
-        } else if (strstr(message.msg_text, "WIN")) {
-            char* winner = strchr(message.msg_text, '\n');
-            cout << "\nGame over! " << (winner ? winner + 1 : "You") << " won!\n";
+        } else if (msg_text.find(protocol_to_str(Protocol::MSG_WIN)) != string::npos) {
+             cout << "\nGame over! " << (msg_text.find(to_string(client_pid)) != string::npos ? "You win!" : "You lose!") << endl;
             break;
-        } else if (strstr(message.msg_text, "DRAW")) {
+        } else if (msg_text.find(protocol_to_str(Protocol::MSG_DRAW)) != string::npos) {
             cout << "\nGame over! It's a draw!" << endl;
             break; 
-        } else if (strstr(message.msg_text, "INVALID")) {
+        } else if (msg_text.find(protocol_to_str(Protocol::MSG_INVALID)) != string::npos) {
             cout << "\nInvalid move! Please try again!" << endl;
         }
+
+    if (msgrcv(msgid, &message, sizeof(msg_t) - sizeof(long), client_pid, 0) == -1) {
+        perror("msgrcv failed --- loop");
+        return 1;
+    }
+
     }
     cout << "Game ended. Press enter to exit ..." << endl;
+    cin.ignore();
+    cin.get();
     return 0;
 }
